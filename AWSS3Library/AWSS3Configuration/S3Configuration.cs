a7687f7092832497126f965a5df103ea1dae85fb
Gabriel Isaac <iamkemical1@gmail.com>
2021-07-04 08:15:45 +0100

    Added S3 Configuration Properties

---
+++ AWSS3Library/AWSS3Configuration/S3Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSS3Library.AWSS3Configuration
+{
+    public class ServiceConfiguration
+    {
+        public AWSS3Configuration AWSS3 { get; set; }
+    }
+    public class AWSS3Configuration
+    {
+        public string BucketName { get; set; }
+    }
+}
--- AWSS3Library/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AWSS3Library
-{
-    public class Class1
-    {
-    }
-}
